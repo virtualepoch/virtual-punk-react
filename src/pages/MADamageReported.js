af4880913c7d311fb8d266738bf272b2276fe58a
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2023-02-21 21:58:48 -0700

    content

--- src/pages/MADamageReported.js
+++ src/pages/MADamageReported.js
@@ -6,10 +6,21 @@ export function MADamageReported() {
         <h1>When the Customer reports damage to their MA Order</h1>
         <h2>&gt; OCC owns ALL Major Appliance damage allowance calls.</h2>
         <p>&gt; If the driver or customer is calling in within 48 hours of delivery, please make sure to own the call.</p>
-        <h1></h1>
-        <p>&gt; </p>
-  
-
+        <h1>NOTE: DAMAGE MUST BE REPORTED WITHIN 48 BUSINESS HOURS.</h1>
+        <p>&gt; Customers are notified of this a the time of delivery as well;..</p>
+        <p>&gt; If the customer is calling outside of 48 hours, they no longer qualify for damage allowance or replacement parts or replacement units.</p>
+        <p>&gt; For further support, please partner with you leader to find the best solution for your customer.</p>
+        <h1>REASON CODE 45 is the ONLY reason code to use for damage allowance markdowns. All other reason codes will negatively impact our stores.</h1>
+        <h1>DO NOT ACCIDENTALLY USE REASON CODE 4</h1>
+        <h1>Markdowns above 25% will be declined by the vendor.</h1>
+        <p>&gt; If the customer asks for damage allowance above 25% then explain it's not possible and recommend replacement part or a replacement unit.</p>
+        <h1>HOW TO DETERMINE DAMAGE ALLOWANCE</h1>
+        <p>&gt; If the driver or customer calls the OCC about a damaged appliance.</p>
+        <p>&gt; Step #1</p>
+        <p>&gt; Click the 'Change' next to 'Store#' in the upper left corner of Order Up and localize to the store on the order. YOU MUST BE PROPERLY LOCALIZED TO HAVE ALL OPTIONS AVAILABLE</p>
+        <p>&gt; Step #2 -After completing the markdown in Order Up you may have to process the refund in SPOS.</p>
+        <p>&gt; Don't forget to complete a 'Markdown Tracking Form' for any markdown other than 'Services Not Eligible for Return in Order Up'.</p>
+        <p>&gt; Step#2</p>
       </div>
     </section>
   );
