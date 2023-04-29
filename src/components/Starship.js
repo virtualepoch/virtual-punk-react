3bca35f2291cd6e209d3b4406a67cf3737478b01
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2023-04-29 10:34:19 -0600

    three testing

--- src/components/Starship.js
+++ src/components/Starship.js
@@ -1,62 +1,82 @@
 import { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera, Float } from "@react-three/drei";
 
-const style = {
-  width: "100%",
-  height: "100%",
-  position: "fixed",
-  top: 0,
-  left: 0,
-  border: "solid aqua",
-  background: "linear-gradient(to right, rgb(0, 50, 50), black, rgb(0, 50, 50))",
-};
+export const Starship = () => {
+  function StarshipGroup() {
+    // const meshRef1 = useRef(null);
+    // const meshRef2 = useRef(null);
+    // const meshRef3 = useRef(null);
+    // const meshRef4 = useRef(null);
 
-function StarshipGroup() {
-  // const meshRef1 = useRef(null);
-  // const meshRef2 = useRef(null);
-  // const meshRef3 = useRef(null);
-  // const meshRef4 = useRef(null);
+    // useFrame(() => {
+    //   if (!meshRef1.current || !meshRef2.current || !meshRef3.current || !meshRef4.current) {
+    //     return;
+    //   }
+    //   meshRef1.current.rotation.x += 0.02;
+    //   meshRef2.current.rotation.x -= 0.02;
+    //   meshRef3.current.rotation.y -= 0.02;
+    //   meshRef4.current.rotation.y += 0.02;
+    // });
 
-  // useFrame(() => {
-  //   if (!meshRef1.current || !meshRef2.current || !meshRef3.current || !meshRef4.current) {
-  //     return;
-  //   }
-  //   meshRef1.current.rotation.x += 0.02;
-  //   meshRef2.current.rotation.x -= 0.02;
-  //   meshRef3.current.rotation.y -= 0.02;
-  //   meshRef4.current.rotation.y += 0.02;
-  // });
+    return (
+      <>
+        {/* NOSE */}
+        <mesh position={[0, 0, -2]} rotation={[-Math.PI / 2, 0, 0]}>
+          <coneGeometry args={[0.5, 4, 3]} />
+          <meshStandardMaterial color={"aqua"} wireframe={false} />
+        </mesh>
+
+        {/* TAIL */}
+        <mesh position={[0, 0.5, 1]} rotation={[Math.PI / 3, Math.PI / 3, 0]}>
+          <coneGeometry args={[0.5, 2, 3]} />
+          <meshStandardMaterial color={"blue"} wireframe={false} />
+        </mesh>
+        <mesh position={[0, 0, 1.2]} rotation={[Math.PI / 2, Math.PI / 3, 0]}>
+          <coneGeometry args={[0.5, 2, 3]} />
+          <meshStandardMaterial color={"purple"} wireframe={false} />
+        </mesh>
+
+        {/* WINGS */}
+        <mesh position={[-2, 0, 0]} rotation={[Math.PI / 2, 0, Math.PI / 2]}>
+          <coneGeometry args={[0.5, 4, 3]} />
+          <meshStandardMaterial color={"red"} wireframe={false} />
+        </mesh>
+        <mesh position={[2, 0, 0]} rotation={[Math.PI / 2, 0, -Math.PI / 2]}>
+          <coneGeometry args={[0.5, 4, 3]} />
+          <meshStandardMaterial color={"red"} wireframe={false} />
+        </mesh>
+
+        {/* THRUSTERS */}
+        <mesh position={[-0.6, 0, 0.6]} rotation={[-Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.1, 0.3, 1]} />
+          <meshStandardMaterial color={"green"} wireframe={false} />
+        </mesh>
+        <mesh position={[0.6, 0, 0.6]} rotation={[-Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.1, 0.3, 1]} />
+          <meshStandardMaterial color={"green"} wireframe={false} />
+        </mesh>
+      </>
+    );
+  }
+
+  const cameraGroup = useRef();
+
+  useFrame(() => {
+    cameraGroup.current.position.z -= 0.2;
+  });
 
   return (
     <>
-      <mesh position={[0, 0, -2]} rotation={[-Math.PI / 2, 0, 0]}>
-        <coneGeometry args={[0.5, 4, 3]} rotateX={Math.PI / 2} />
-        <meshStandardMaterial color={"aqua"} wireframe={false} />
-      </mesh>
-      <mesh position={[0, 0.5, 1]} rotation={[Math.PI / 3, Math.PI / 3, 0]}>
-        <coneGeometry args={[0.5, 2, 3]} rotateX={Math.PI / 2} />
-        <meshStandardMaterial color={"blue"} wireframe={false} />
-      </mesh>
-      <mesh position={[-2, 0, 0]} rotation={[Math.PI / 2, 0, Math.PI / 2]}>
-        <coneGeometry args={[0.5, 4, 3]} rotateX={Math.PI / 2} />
-        <meshStandardMaterial color={"red"} wireframe={false} />
-      </mesh>
-      <mesh position={[2, 0, 0]} rotation={[Math.PI / 2, 0, -Math.PI / 2]}>
-        <coneGeometry args={[0.5, 4, 3]} rotateX={Math.PI / 2} />
-        <meshStandardMaterial color={"red"} wireframe={false} />
-      </mesh>
+      <OrbitControls />
+      <group ref={cameraGroup}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[0, 15, 10]} angle={0.3} />
+        <PerspectiveCamera position={[0, 0, 15]} fov={40} makeDefault />
+        <Float floatIntensity={2} speed={2}>
+          <StarshipGroup />
+        </Float>
+      </group>
     </>
   );
-}
-
-export function Starship() {
-  return (
-    <Canvas style={style} camera={{ position: [0, 0, 3] }}>
-      <OrbitControls />
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 15, 10]} angle={0.3} />
-      <StarshipGroup />
-    </Canvas>
-  );
-}
+};
--- src/pages/StarPunk.js
+++ src/pages/StarPunk.js
@@ -1,20 +1,55 @@
 import { Starship } from "../components/Starship";
+import { Canvas } from "@react-three/fiber";
 
 export function StarPunk() {
   const style = {
-    position: "absolute",
-    top: "80px",
-    left: 0,
-    zIndex: 4,
-    color: "white",
-    padding: "10px",
-    border: "solid aqua",
+    h1: {
+      position: "absolute",
+      top: "80px",
+      left: 0,
+      zIndex: 4,
+      color: "white",
+      padding: "10px",
+      border: "solid aqua",
+    },
+    canvas: {
+      width: "100%",
+      height: "100%",
+      position: "fixed",
+      top: 0,
+      left: 0,
+      border: "solid aqua",
+      background: "linear-gradient(to right, rgb(0, 50, 50), black, rgb(0, 50, 50))",
+    },
   };
+
+  function ExtendingWalls() {
+    const ref = useRef(null);
+
+    // useFrame(() => {
+    //   if (ref.current.position.z < 104) {
+    //     ref.current.position.z += 0.008;
+    //   } else {
+    //     ref.current.position.z -= 124;
+    //   }
+    // });
+
+    return (
+      <mesh ref={ref} position={[0, 0, -20]} rotation={[Math.PI / -2, Math.PI / 4, 0]}>
+        <cylinderGeometry args={[55, 55, 1120, 4, 500]} />
+        <meshBasicMaterial color={"aqua"} wireframe={true} />
+      </mesh>
+    );
+  }
 
   return (
     <>
-      <h1 style={style}>Star Punk</h1>
-      <Starship />
+      <h1 style={style.h1}>Star Punk</h1>
+      <Canvas style={style.canvas}>
+        <OrbitControls />
+        <ExtendingWalls />
+        <Starship />
+      </Canvas>
     </>
   );
 }
