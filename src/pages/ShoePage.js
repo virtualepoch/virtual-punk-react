8666aa090d98462523cfcedf19fe76941aeabf4c
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2023-05-10 07:35:21 -0600

    ShoePage.js mobile testing

--- src/pages/ShoePage.js
+++ src/pages/ShoePage.js
@@ -8,12 +8,14 @@ const style = {
   canvas: {
     width: "100%",
     height: "100vh",
-    border: "solid aqua",
+    background: "aqua",
+    position: "fixed",
+    top: "70px",
   },
   picker: {
     position: "fixed",
-    top: "80px",
-    left: "20px",
+    top: "70px",
+    left: "0",
     border: "solid aqua",
   },
 };
@@ -38,11 +40,12 @@ function Shoe() {
   const { nodes, materials } = useGLTF("models/shoe-draco.glb");
   const [hovered, set] = useState(null);
 
-  useFrame((state) => {
-    const t = state.clock.getElapsedTime();
-    ref.current.rotation.set(Math.cos(t / 4) / 8, Math.sin(t / 4) / 8, -0.2 - (1 + Math.sin(t / 1.5)) / 20);
-    ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10;
-  });
+  // HOVER EFFEECT VVVVVVVVVVVVVVVVVVVV
+  // useFrame((state) => {
+  //   const t = state.clock.getElapsedTime();
+  //   ref.current.rotation.set(Math.cos(t / 4) / 8, Math.sin(t / 4) / 8, -0.2 - (1 + Math.sin(t / 1.5)) / 20);
+  //   ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10;
+  // });
 
   useEffect(() => {
     const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`;
@@ -71,7 +74,7 @@ function Picker() {
   const snap = useSnapshot(state);
   return (
     <div style={{ display: snap.current ? "block" : "none" }}>
-      <HexColorPicker className="picker" color={snap.items[snap.current]} onChange={(color) => (state.items[snap.current] = color)} />
+      <HexColorPicker style={style.picker} className="picker" color={snap.items[snap.current]} onChange={(color) => (state.items[snap.current] = color)} />
       <h1>{snap.current}</h1>
     </div>
   );
