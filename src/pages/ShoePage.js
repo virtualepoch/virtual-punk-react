90e9a3f1177841d67508c2392a8c318d1e8b6c94
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2023-05-11 07:41:30 -0600

    mobile testing

--- src/pages/ShoePage.js
+++ src/pages/ShoePage.js
@@ -58,14 +58,15 @@ function Shoe() {
 
   return (
     <group ref={ref} onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))} onPointerOut={(e) => e.intersections.length === 0 && set(null)} onPointerMissed={() => (state.current = null)} onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
-      <mesh receiveShadow castShadow geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.items.inner} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.items.sole} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.items.stripes} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.items.band} />
-      <mesh receiveShadow castShadow geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.items.patch} />
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.items.inner} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.items.sole} />
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.items.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.items.band} />
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.items.patch} />
+      {/* NOTE REMOVED recieveShadow and castShadow FROM MESHES */}
     </group>
   );
 }
@@ -87,8 +88,8 @@ export function ShoePage() {
         <ambientLight intensity={0.7} />
         <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
         <Shoe />
-        <Environment preset="city" />
-        <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
+        {/* <Environment preset="city" /> */}
+        <ContactShadows position={[0, -0.8, 0]} opacity={0.55} scale={10} blur={1} far={0.8} />
         <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={true} enablePan={false} />
       </Canvas>
       <Picker style={style.picker} />
