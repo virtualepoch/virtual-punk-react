55fb301405b2ce589e5dfb187c09f400726f49e5
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2023-05-27 22:02:16 -0600

    three.js testing

--- src/pages/Testing.js
+++ src/pages/Testing.js
@@ -1,17 +1,19 @@
 import * as THREE from "three";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { Model } from "../components/models/Ayanami_rei";
+import { OrbitControls, PerspectiveCamera, Stars } from "@react-three/drei";
+import { Model } from "../components/models/SolarSystem";
 import { PillLinks } from "../components/PillLinks";
 
 export function Testing() {
   return (
     <>
       <h1 className="page-title">Testing</h1>
-      <Canvas className="canvas testing" camera={{ position: [0, 0, 2] }}>
+      <Canvas className="canvas" camera={{ position: [0, 0, -3] }}>
         <ambientLight intensity={1} />
         <OrbitControls />
-        <Model position={[0, -1, 0]} />
+        <Model />
+        <Stars />
+        <PerspectiveCamera position={[250, 0, 0]} rotation={[0, 0, 0]} fov={80} makeDefault far={5000} />
       </Canvas>
       <PillLinks backTo={"/"} backName={"home"} forwardTo={"/torus"} forwardName={"torus"} />
     </>
