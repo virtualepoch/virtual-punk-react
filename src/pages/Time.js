e1a4d3bfdd674c5918d3330ad964b41e651944e9
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2023-06-24 12:56:45 -0600

    Time.js edit

--- src/pages/Time.js
+++ src/pages/Time.js
@@ -1,50 +1,49 @@
 import { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 import { PillLinks } from "../components/PillLinks";
-
-const style = {
-  width: "100%",
-  height: "100%",
-  position: "fixed",
-  top: 0,
-  left: 0,
-  background: "linear-gradient(to right, rgb(0, 50, 50), black, rgb(0, 50, 50))",
-};
 
 function CubeGroup() {
   const meshRef1 = useRef(null);
   const meshRef2 = useRef(null);
   const meshRef3 = useRef(null);
   const meshRef4 = useRef(null);
+  const meshRef5 = useRef(null);
+
+  const rotateSpeed = 0.015;
 
   useFrame(() => {
-    if (!meshRef1.current || !meshRef2.current || !meshRef3.current || !meshRef4.current) {
+    if (!meshRef1.current || !meshRef2.current || !meshRef3.current || !meshRef4.current || !meshRef4.current) {
       return;
     }
-    meshRef1.current.rotation.x += 0.01;
-    meshRef2.current.rotation.x -= 0.01;
-    meshRef3.current.rotation.x -= 0.01;
-    meshRef4.current.rotation.x += 0.01;
+
+    meshRef1.current.rotation.x += rotateSpeed;
+    meshRef2.current.rotation.x -= rotateSpeed;
+    meshRef3.current.rotation.x += rotateSpeed;
+    meshRef4.current.rotation.x -= rotateSpeed;
+    meshRef5.current.rotation.x += rotateSpeed;
   });
 
   return (
     <>
       <mesh ref={meshRef1} position={[-2, 0, 0]}>
         <boxBufferGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="aqua" wireframe={false} />
+        <meshStandardMaterial color="cyan" wireframe={false} />
       </mesh>
       <mesh ref={meshRef2} position={[-1, 0, 0]}>
         <boxBufferGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="aqua" wireframe={false} />
+        <meshStandardMaterial color="cyan" wireframe={false} />
       </mesh>
-      <mesh ref={meshRef3} position={[1, 0, 0]}>
+      <mesh ref={meshRef3} position={[0, 0, 0]}>
         <boxBufferGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="aqua" wireframe={false} />
+        <meshStandardMaterial color="cyan" wireframe={false} />
       </mesh>
-      <mesh ref={meshRef4} position={[2, 0, 0]}>
+      <mesh ref={meshRef4} position={[1, 0, 0]}>
         <boxBufferGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="aqua" wireframe={false} />
+        <meshStandardMaterial color="cyan" wireframe={false} />
+      </mesh>
+      <mesh ref={meshRef5} position={[2, 0, 0]}>
+        <boxBufferGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="cyan" wireframe={false} />
       </mesh>
     </>
   );
@@ -54,10 +53,11 @@ export function Time() {
   return (
     <>
       <h1 className="page-title">Time</h1>
-      <Canvas style={style} camera={{ position: [0, 0, 5] }}>
-        {/* <OrbitControls /> */}
-        {/* <ambientLight intensity={0.5} /> */}
+      <Canvas className="canvas" camera={{ position: [0, 0, 5] }}>
         <directionalLight position={[0, 15, 10]} angle={0.3} />
+        <group position={[0, -3, 0]}>
+          <CubeGroup />
+        </group>
         <group position={[0, -2, 0]}>
           <CubeGroup />
         </group>
@@ -75,9 +75,6 @@ export function Time() {
           <CubeGroup />
         </group>
         <group position={[0, 4, 0]}>
-          <CubeGroup />
-        </group>
-        <group position={[0, -3, 0]}>
           <CubeGroup />
         </group>
       </Canvas>
