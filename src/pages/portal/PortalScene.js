12ca4ad02508a47f1e066a6038ce096609bcbe07
virtual_epoch <76543678+virtualepoch@users.noreply.github.com>
2023-07-04 17:04:17 -0600

    testing / cleanup

--- src/pages/portal/PortalScene.js
+++ src/pages/portal/PortalScene.js
@@ -1,19 +1,17 @@
 import * as THREE from "three";
 import { useState, useRef, useEffect } from "react";
-import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
-import { CameraControls, Stars, Float, MeshPortalMaterial, RoundedBox, useTexture, Text } from "@react-three/drei";
-import scenicWater from "../../assets/images/scenic_underwater.jpg";
-import realisticJungle from "../../assets/images/realistic_jungle.jpg";
-import scenicJungle from "../../assets/images/scenic_jungle.jpg";
-import dreamlike from "../../assets/images/dreamlike_cyber_punk.jpg";
-import realistic from "../../assets/images/realistic_cyber_punk.jpg";
-import sky from "../../assets/images/sky_cyber_punk.jpg";
-import tech from "../../assets/images/tech_noir__cyberpunk_cyber_punk.jpg";
+import { useFrame, useLoader, useThree } from "@react-three/fiber";
+import { CameraControls, MeshPortalMaterial, RoundedBox, Text } from "@react-three/drei";
+import { easing } from "maath";
+
+import bg from "../../assets/images/realistic_wireframe_matrix.jpg";
+import raptorBg from "../../assets/images/realistic_clowdy_jungle_floor.jpg";
+import trexBg from "../../assets/images/realistic_dark_desert.jpg";
+import megBg from "../../assets/images/scenic_underwater.jpg";
 
 import { Raptor } from "./Raptor";
 import { Trex } from "./Trex";
 import { Megalodon } from "./Megalodon";
-import { easing } from "maath";
 
 export function PortalScene() {
   const [active, setActive] = useState(null);
@@ -34,20 +32,33 @@ export function PortalScene() {
   return (
     <>
       <ambientLight intensity={1} />
-
+      <Background />
       <CameraControls ref={controlsRef} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 3} />
-      <CreatureCard width={2} height={3} texture={scenicJungle} position={[-2.7, 0, 0.5]} rotation-y={Math.PI / 8} textY={-1.4} name={"Velociraptor"} fontSize={0.3} active={active} setActive={setActive} hovered={hovered} setHovered={setHovered}>
-        <Raptor position={[0, -1, 0]} hovered={hovered === "Velociraptor"} />
+
+      <CreatureCard width={2} height={4} texture={raptorBg} position={[-2.8, 0, 0.5]} rotation-y={Math.PI / 8} textY={-1.4} name={"Velociraptor"} fontSize={0.3} active={active} setActive={setActive} hovered={hovered} setHovered={setHovered}>
+        <Raptor position={[0, -1.2, 0]} hovered={hovered === "Velociraptor"} />
       </CreatureCard>
-      <CreatureCard width={4} height={6} texture={realisticJungle} textY={-2.7} name={"Tyranosaurus Rex"} fontSize={0.5} active={active} setActive={setActive}>
-        <Trex position-y={-3} scale={3} />
+
+      <CreatureCard width={4} height={6} texture={trexBg} textY={-2.7} name={"Tyranosaurus Rex"} fontSize={0.5} active={active} setActive={setActive}>
+        <Trex position={[0, -3, -2]} scale={3} />
       </CreatureCard>
-      <CreatureCard width={2} height={3} texture={scenicWater} position={[2.7, 0, 0.5]} rotation-y={Math.PI / -8} textY={-1.4} name={"Megalodon"} fontSize={0.3} active={active} setActive={setActive}>
+
+      <CreatureCard width={2} height={4} texture={megBg} position={[2.8, 0, 0.5]} rotation-y={Math.PI / -8} textY={-1.4} name={"Megalodon"} fontSize={0.3} active={active} setActive={setActive}>
         <Megalodon position={[0, -0.8, 0]} scale={1.5} />
       </CreatureCard>
     </>
   );
 }
+
+const Background = () => {
+  const map = useLoader(THREE.TextureLoader, bg);
+  return (
+    <mesh position={[0, 0, 0]} rotation={[0, 1.6, 0]}>
+      <sphereGeometry args={[14, 50, 10]} />
+      <meshStandardMaterial map={map} side={THREE.BackSide} />
+    </mesh>
+  );
+};
 
 const CreatureCard = ({ width, height, children, texture, textY, fontSize, name, active, setActive, hovered, setHovered, ...props }) => {
   const map = useLoader(THREE.TextureLoader, texture);
@@ -66,10 +77,11 @@ const CreatureCard = ({ width, height, children, texture, textY, fontSize, name,
       <RoundedBox args={[width, height, 0.1]} onClick={() => setActive(active === name ? null : name)} name={name} onPointerEnter={() => setHovered(name)} onPointerLeave={() => setHovered(null)}>
         <MeshPortalMaterial ref={portalRef}>
           <ambientLight intensity={1} />
+          <pointLight />
           <directionalLight position={[10, 15, 10]} angle={0.3} />
           {children}
-          <mesh position={[0, 0, 0]}>
-            <sphereGeometry args={[5, 10, 10]} />
+          <mesh position={[0, 0, 0]} rotation={[0, 2, 0]}>
+            <sphereGeometry args={[4, 10, 10]} />
             <meshStandardMaterial map={map} side={THREE.BackSide} />
           </mesh>
         </MeshPortalMaterial>
