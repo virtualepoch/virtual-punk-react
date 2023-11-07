4ff33754fd1adb92f004eb8d226f7978f44aaa3e
Virtual Epoch <cdk11235@gmail.com>
2023-11-07 14:14:47 -0800

    bday edits

--- src/pages/bday-cards/SandyBday.js
+++ src/pages/bday-cards/SandyBday.js
@@ -1,9 +1,26 @@
 import { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Cake } from "./Cake";
+import * as THREE from "three";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import bg from "../../assets/images/balloons_cgi.jpg";
+import { StrawCake } from "./StrawCake";
 
 export function SandyBday() {
-  var torusRotSpeed = 0.01;
+  const BackDrop = () => {
+    const map = useLoader(THREE.TextureLoader, bg);
+    const ref = useRef();
+
+    useFrame(() => {
+      ref.current.rotation.y += 0.0003;
+    });
+
+    return (
+      <mesh position={[0, 0, 0]} rotation={[0, 1.6, 0]} ref={ref}>
+        <sphereGeometry args={[27, 20, 4]} />
+        <meshStandardMaterial map={map} side={THREE.BackSide} />
+      </mesh>
+    );
+  };
+  var torusRotSpeed = 0.001;
 
   function TorusGroup() {
     const meshRef1 = useRef(null);
@@ -22,27 +39,25 @@ export function SandyBday() {
       }
       meshRef1.current.rotation.z += torusRotSpeed;
       meshRef2.current.rotation.z -= torusRotSpeed;
-      meshRef3.current.rotation.z += torusRotSpeed;
-      meshRef4.current.rotation.z -= torusRotSpeed;
     });
 
     return (
       <mesh position-z={-1}>
         <mesh ref={meshRef1} position={[0, 0, 0]}>
-          <torusGeometry args={[0.9, 0.1, 4, 33]} />
-          <meshStandardMaterial color="crimson" wireframe={true} />
+          <torusGeometry args={[0.9, 0.03, 4, 33]} />
+          <meshStandardMaterial color="hotpink" wireframe={true} />
         </mesh>
-        <mesh ref={meshRef2} position={[0, 0, 0]}>
-          <torusGeometry args={[1.1, 0.1, 4, 33]} />
+        <mesh ref={meshRef2} position={[0, 0, 0.5]}>
+          <torusGeometry args={[1.1, 0.03, 4, 33]} />
           <meshStandardMaterial color="aqua" wireframe={true} />
         </mesh>
-        <mesh ref={meshRef3} position={[0, 0, 0]}>
+        <mesh ref={meshRef3} position={[0, 0, 1.75]}>
           <torusGeometry args={[1.3, 0.1, 4, 33]} />
-          <meshStandardMaterial color="crimson" wireframe={true} />
+          <meshStandardMaterial color="hotpink" wireframe={false} />
         </mesh>
-        <mesh ref={meshRef4} position={[0, 0, 0]}>
-          <torusGeometry args={[1.5, 0.1, 4, 33]} />
-          <meshStandardMaterial color="aqua" wireframe={true} />
+        <mesh ref={meshRef4} position={[0, 0, 2.1]}>
+          <torusGeometry args={[1.5, 0.15, 4, 33]} />
+          <meshStandardMaterial color="aqua" wireframe={false} />
         </mesh>
       </mesh>
     );
@@ -56,16 +71,18 @@ export function SandyBday() {
     });
     return (
       <mesh ref={cake} position={[0, -1.5, 0]}>
-        <Cake />
+        <StrawCake />
       </mesh>
     );
   }
 
   return (
     <>
-      <h1 className="page-title">Happy Birthday Sandy!</h1>
+      <h1 className="page-title happy-bday">Happy Birthday Sandy!</h1>
       <Canvas className="canvas torus" camera={{ position: [0, 0, 3] }}>
         <ambientLight intensity={1} />
+        <pointLight intensity={1} />
+        <BackDrop />
         <TorusGroup />
         <CakeSpin />
       </Canvas>
